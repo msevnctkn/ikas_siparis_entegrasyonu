0e97bd67469349b70ac9ccf6bc8fa412e91950d4
Muhammed Sevinçtekin <50045401+msevnctkn@users.noreply.github.com>
2026-04-04 08:55:26 +0300

    Update ikas_entegrasyon.py

--- ikas_entegrasyon.py
+++ ikas_entegrasyon.py
@@ -150,7 +150,7 @@ class IKAS_SIPARIS_ENTEGRASYON():
           siparis_durumu = content["orderLineItems"][0]["status"]
        
           sonuc = self.resmi_tatil.is_gunu_ekle(siparis_tarihi,12)
-          st.write("tarih:", siparis_tarih2, type(siparis_tarihi))
+          st.write("tarih:", siparis_tarihi, type(siparis_tarihi))
           st.write("is_gunu_ekle sonucu:", sonuc, type(sonuc))
 
           imalat_bitis_suresi2 = pd.to_datetime(sonuc, errors="coerce")
